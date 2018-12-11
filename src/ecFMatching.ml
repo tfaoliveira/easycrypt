d96e54289bfd6c748579d3087cd3593c30a26860
Cécile BARITEL-RUET <cecile.bareitel-ruet@inria.fr>
2018-12-11 11:30:31 +0100

    .

--- src/ecFMatching.ml
+++ src/ecFMatching.ml
@@ -1002,9 +1002,9 @@ let rec process (e : engine) : nengine =
   let _ =
     let ppe = e.e_env.env_ppe in
     Format.eprintf "[W]?? %s delta_p and %s delta_a : %a =? %a\n"
-      (if e.e_env.env_red_info_p.EcReduction.delta_h (EcIdent.create "foobar")
+      (if e.e_env.env_red_info_p.EcReduction.delta_p (EcPath.psymbol "foobar")
        then "with" else "without")
-      (if e.e_env.env_red_info_a.EcReduction.delta_h (EcIdent.create "foobar")
+      (if e.e_env.env_red_info_a.EcReduction.delta_p (EcPath.psymbol "foobar")
        then "with" else "without")
       (EcPrinting.pp_pattern ppe) e.e_pattern
       (EcPrinting.pp_pat_axiom ppe) e.e_head
