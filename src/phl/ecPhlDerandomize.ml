39256abd7a9ce3ee94f8998880c7ac2d4a922625
Pierre-Yves Strub <pierre-yves@strub.nu>
2019-02-05 11:50:37 +0100

    derandomize: add ll conditions

--- src/phl/ecPhlDerandomize.ml
+++ src/phl/ecPhlDerandomize.ml
@@ -20,11 +20,11 @@ exception Failure
 
 (* -------------------------------------------------------------------- *)
 let derandomize env =
-  let rec aux0 infos s =
-    aux infos (List.rev s.s_node)
+  let rec aux0 ?(top = false) infos s =
+    aux top infos (List.rev s.s_node)
 
-  and aux (me, (rd0, wr0)) s =
-    match s with [] -> me, ([], []) | i :: s ->
+  and aux top (me, (rd0, wr0)) s =
+    match s with [] -> me, ([], [], []) | i :: s ->
 
     try
       match i.i_node with
@@ -36,7 +36,7 @@ let derandomize env =
 
           if pv.pv_kind <> PVloc then raise Failure;
 
-          let me, (rnds, body) = aux (me, (rd0, wr0)) s in
+          let me, (rnds, ll, body) = aux top (me, (rd0, wr0)) s in
 
           let rd = EcPV.s_read_r  env rd0 (stmt s) in
           let wr = EcPV.s_write_r env wr0 (stmt s) in
@@ -53,7 +53,9 @@ let derandomize env =
             if pv_equal pv pv' then None else
             Some (i_asgn (LvVar (pv, pvty), e_var pv' pvty)) in
 
-          me, (((pv', pvty), e) :: rnds, List.ocons asgn body)
+          let ll = if top then ll else e :: ll in
+
+          me, (((pv', pvty), e) :: rnds, ll, List.ocons asgn body)
         end
 
       | Sif (e, s1, s2) ->
@@ -62,32 +64,33 @@ let derandomize env =
           let rd2 = EcPV.PV.union rd0 (EcPV.s_read  env s1) in
           let wr2 = EcPV.PV.union rd0 (EcPV.s_write env s1) in
 
-          let me, (rnds, body) = aux (me, (rd0, wr0)) s in
+          let me, (rnds, ll, body) = aux top (me, (rd0, wr0)) s in
 
-          let me, (rnds1, body1) = aux0 (me, (rd1, wr1)) s1 in
-          let me, (rnds2, body2) = aux0 (me, (rd2, wr2)) s2 in
+          let me, (rnds1, ll1, body1) = aux0 (me, (rd1, wr1)) s1 in
+          let me, (rnds2, ll2, body2) = aux0 (me, (rd2, wr2)) s2 in
 
           let body1 = List.rev body1 in
           let body2 = List.rev body2 in
 
-          me, ((rnds1 @ rnds2 @ rnds), i_if (e, stmt body1, stmt body2) :: body)
+          me, ((rnds1 @ rnds2 @ rnds), (ll1 @ ll2 @ ll),
+               i_if (e, stmt body1, stmt body2) :: body)
 
       | _ -> raise Failure
 
     with Failure ->
-      let me, (rnds, body) = aux (me, (rd0, wr0)) s in
-      me, (rnds, i :: body)
+      let me, (rnds, ll, body) = aux top (me, (rd0, wr0)) s in
+      me, (rnds, ll, i :: body)
 
   in
 
   fun me s ->
-    let me, (rnds, body) =
-      aux0 (me, (EcPV.PV.empty, EcPV.PV.empty)) s in
+    let me, (rnds, ll, body) =
+      aux0 ~top:true (me, (EcPV.PV.empty, EcPV.PV.empty)) s in
 
     let rnds = List.map (fun ((pv, pvty), e) ->
       i_rnd (LvVar (pv, pvty), e)) (List.rev rnds) in
 
-    (me, stmt (rnds @ List.rev body))
+    (me, ll, stmt (rnds @ List.rev body))
 
 (* -------------------------------------------------------------------- *)
 let t_derandomize tc =
@@ -97,9 +100,17 @@ let t_derandomize tc =
   match concl.f_node with
   | FhoareS hs -> begin
       try
-        let me, body = derandomize env hs.hs_m hs.hs_s in
+        let me, ll, body = derandomize env hs.hs_m hs.hs_s in
+        let ll =
+          List.map
+            (fun e ->
+              f_forall_simpl [fst me, GTmem (snd me)]
+                (f_lossless
+                   (oget (as_tdistr (EcEnv.Ty.hnorm e.e_ty env)))
+                   (form_of_expr (fst me) e)))
+            ll in
         let concl = { hs with hs_m = me; hs_s = body; } in
-        FApi.xmutate1 tc `Dernd [EcFol.f_hoareS_r concl]
+        FApi.xmutate1 tc `Dernd (ll @ [EcFol.f_hoareS_r concl])
 
       with Failure -> EcLowGoal.t_id tc
     end
