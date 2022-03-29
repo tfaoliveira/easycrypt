32578dad2c74e420fc9400dfc0b203c6f740dd81
Pierre-Yves Strub <pierre-yves@strub.nu>
2022-03-29 10:01:08 +0200

    License change: CeCILL B/C -> MIT

--- src/ecCHoare.ml
+++ src/ecCHoare.ml
@@ -1,11 +1,5 @@
-(* --------------------------------------------------------------------
- * Copyright (c) - 2012--2016 - IMDEA Software Institute
- * Copyright (c) - 2012--2018 - Inria
- * Copyright (c) - 2012--2018 - Ecole Polytechnique
- *
- * Distributed under the terms of the CeCILL-C-V1 license
- * -------------------------------------------------------------------- *)
-
+(* -------------------------------------------------------------------- *)
+(* Function for cost                                                    *)
 open EcUtils
 open EcTypes
 open EcFol
