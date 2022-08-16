e26e48edaacaf11448d7c89e4594cb98898ec02a
Adrien Koutsos <adrien.koutsos@inria.fr>
2022-08-16 10:33:12 +0200

    Adding missing `mli` and theory files

---
+++ src/ecBaseLogic.mli
@@ -0,0 +1,47 @@
+(* -------------------------------------------------------------------- *)
+open EcTypes
+open EcCoreFol
+
+(* -------------------------------------------------------------------- *)
+module Epoch : sig
+  type t
+
+  val init : t
+
+  val lt  : t -> t -> bool
+  val leq : t -> t -> bool
+
+  val next : t -> t
+  val max : t -> t -> t
+  val min : t -> t -> t
+end
+
+(* -------------------------------------------------------------------- *)
+type local_kind =
+| LD_var    of ty * form option
+| LD_mem    of EcMemory.memtype
+| LD_modty  of mod_ns * EcModules.module_type
+| LD_hyp    of form
+| LD_abs_st of EcModules.abs_uses
+
+(* -------------------------------------------------------------------- *)
+type l_local = {
+  l_id    : EcIdent.t;
+  l_epoch : Epoch.t;
+  l_kind  : local_kind;
+}
+
+type l_locals = l_local list
+
+val l_id    : l_local -> EcIdent.t
+val l_epoch : l_local -> Epoch.t
+
+(* -------------------------------------------------------------------- *)
+type hyps = {
+  h_tvar  : EcDecl.ty_params;
+  h_local : l_local list;
+}
+
+val hyps_epoch : hyps -> Epoch.t
+
+val by_id_opt : EcIdent.t -> hyps -> l_local option
---
+++ src/ecCHoare.mli
@@ -0,0 +1,43 @@
+open EcFol
+open EcPath
+open EcSymbols
+
+(* -------------------------------------------------------------------- *)
+val check_loaded : EcEnv.env -> unit
+
+(* -------------------------------------------------------------------- *)
+val cost_orcl : symbol -> xpath -> form -> form
+
+(* -------------------------------------------------------------------- *)
+type csub_res = { cond : form; res : form; }
+
+(* Backward reasoning on cost.
+   Looks for a solution [x] of [c = x + sub]. *)
+val cost_sub : c:form -> sub:form -> csub_res
+
+(* Same as [cost_sub], but only for the concrete cost.
+   [c] of type [tcost], [sub] of type [xint]. *)
+val cost_sub_self : c:form -> sub:form -> csub_res
+
+(* -------------------------------------------------------------------- *)
+(* [c] of type [tcost], [a] of type [xint] *)
+val cost_add_self : c:form -> a:form -> form
+
+(* -------------------------------------------------------------------- *)
+(* [choare_xsum cost (m,n)]:
+   [cost] of type [tcost], [m] of type [tint], [n] of type [txint].
+
+   [n] must be finite, i.e. [n = f_N n_fin]. *)
+val choare_xsum : form -> (form * form) -> form
+
+(* -------------------------------------------------------------------- *)
+val free_expr : EcTypes.expr -> bool
+
+(* The cost of an expression evaluation in any memory of type [menv]
+   satisfying [pre].
+   Type [xint]. *)
+val cost_of_expr : EcFol.form -> EcMemory.memenv -> EcTypes.expr -> EcFol.form
+
+(* The cost of an expression evaluation in any memory of type [menv].
+   Type [xint]. *)
+val cost_of_expr_any : EcMemory.memenv -> EcTypes.expr -> EcFol.form
